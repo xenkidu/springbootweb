676425d480d052390067511e61572b02fb95f2fe
xenkidu <aburton85@live.com>
2020-10-31 17:08:49 -0500

    Updated controller with Post

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -2,15 +2,18 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Message;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class MessageController {
 
-    @ResponseBody
     @GetMapping("/message")
     Message send() {
         return new Message("first message");
+    }
+
+    @PostMapping("/message")
+    Message echo (@RequestBody Message message) {
+        return message;
     }
 }
